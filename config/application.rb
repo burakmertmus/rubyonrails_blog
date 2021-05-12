3ed887cc6beb36aa22c5e64b9571ccbc2dc84b55
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-12 16:04:58 +0300

    Config update

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+config.assets.initialize_on_precompile = false
 require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +11,6 @@ module Weblog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
-    config.assets.initialize_on_precompile = false
+    
   end
 end
