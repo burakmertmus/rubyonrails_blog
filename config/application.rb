2522c9345896ef4b2537b809583867ca98b7871e
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-12 15:36:41 +0300

    Config changed

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module Weblog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+    config.assets.enabled = false
+    config.assets.initialize_on_precompile = false
   end
 end
