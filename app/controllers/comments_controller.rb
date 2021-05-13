33dc118bb3a1b08db71a2e042bcdfddd7735df0d
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-13 14:26:44 +0300

    platform controlls updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,9 @@
 class CommentsController < ApplicationController
 	before_action :set_post
-	#skip_before_action :verify_authenticity_token
+	skip_before_action :verify_authenticity_token
 	
 	def create
 		comment = @post.comments.create! comments_params
-		#@comment =Comment.create! text: params[:comment][:text], article: @article
 		CommentsMailer.submitted(comment).deliver_later
 		CommentsChannel.broadcast(comment)
 		
