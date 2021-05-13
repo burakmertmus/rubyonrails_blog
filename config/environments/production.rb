6dec9878ede8fc16c24d074094ab90884577f5b7
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-13 19:04:18 +0300

    precompile deleted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.precompile = false
+  #config.assets.precompile = false
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
-  config.action_cable.url = "wss://agile-refuge-21209.herokuapp.com/cable" 
-  config.action_cable.allowed_request_origins = ['http://agile-refuge-21209.herokuapp.com', /http:\/\/agile-refuge-21209.herokuapp.com*/]
+  config.action_cable.url = "wss://agile-refuge-21209.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['http://agile-refuge-21209.herokuapp.com', /http:\/\/agile-refuge-21209.herokuapp.*/]
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
