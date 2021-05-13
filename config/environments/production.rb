97805cef77bd95ed3bdd8df6dd9c30557020434a
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-13 18:44:54 +0300

    localhost config back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "weblog_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_cable.disable_request_forgery_protection = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
