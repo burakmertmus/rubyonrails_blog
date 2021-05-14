e3a25814afcd16833268f2f311b4f83fee0b921e
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-14 21:55:23 +0300

    index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-	root to: "home#index"
 	  resources :posts do
 	  	resources :comments
 	  end
-	end
+	  draw("https://agile-refuge-21209.herokuapp.com/posts")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
