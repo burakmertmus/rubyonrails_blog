a90a04f8480bc674094e2a36f577658c8f9b59e3
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-14 18:56:46 +0300

    index.html

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,11 +11,11 @@ class CommentsController < ApplicationController
 	end
 
 
-	def destroy
-		@comment = @post.comments.find_by (params [: id])
-		@post.comment.destroy
-		redirect_to @post
-	end
+	#def destroy
+	#	@comment = @post.comments.find_by (params [: id])
+	#	@post.comment.destroy
+	#	redirect_to @post
+	#end
 	
 	private
 		def set_post
