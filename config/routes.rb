568de17ac789351c91b4507a8ea01367ace8a0db
Burak Mert Mus <burakmertmus@gmail.com>
2021-05-14 22:12:32 +0300

    index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-	direct :homepage do
-  		"https://agile-refuge-21209.herokuapp.com/posts"
-	end
 	  resources :posts do
 	  	resources :comments
 	  end
-	  draw(:admin)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
